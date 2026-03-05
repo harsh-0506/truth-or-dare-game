a0fbab894993fb3bcbdf0f01cbef35b818080774
harsh-0506 <weirdoharshjais@gmail.com>
2026-03-05 14:51:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const io = require('socket.io')(http);
 
 const gameData = {}; 
 
+// This line tells the server it is allowed to share the .mp3 files!
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,90 +19,118 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   let myRoom = ''; 
 
-  socket.on('createRoom', () => {
+  // NEW: We now receive an object with both the mode AND the player's name
+  socket.on('createRoom', (data) => {
     myRoom = Math.random().toString(36).substring(2, 6).toUpperCase();
     socket.join(myRoom);
     
-    // Create the memory bank, and add Player 1's ID to the player list
     gameData[myRoom] = { 
-        allDares: [], 
+        mode: data.mode, 
         playersReady: 0,
-        players: [socket.id], // Stores the unique walkie-talkie IDs
-        turnIndex: 0 // 0 means Player 1, 1 means Player 2
+        players: [socket.id], 
+        playerNames: { [socket.id]: data.name || "Player 1" }, // Stores the creator's name
+        turnIndex: 0,
+        playerDares: {}, 
+        combinedDares: [] 
     };
     
     socket.emit('roomCreated', myRoom);
   });
 
-  socket.on('joinRoom', (roomCode) => {
-    myRoom = roomCode; 
+  socket.on('joinRoom', (data) => {
+    myRoom = data.roomCode; 
     socket.join(myRoom);
     
-    // Add Player 2's ID to the player list
     if (gameData[myRoom]) {
         gameData[myRoom].players.push(socket.id);
+        gameData[myRoom].playerNames[socket.id] = data.name || "Player 2"; // Stores the joiner's name
     }
-
-    io.to(myRoom).emit('gameReady'); 
+    
+    // Send the names list back to everyone so the screens update
+    io.to(myRoom).emit('gameReady', gameData[myRoom].playerNames); 
   });
 
   socket.on('submitDares', (playerDares) => {
-    // Combine the arrays and mix them up
-    gameData[myRoom].allDares.push(...playerDares);
-    gameData[myRoom].allDares.sort(() => Math.random() - 0.5); // A simple trick to shuffle the dares!
-
+    gameData[myRoom].playerDares[socket.id] = playerDares.sort(() => Math.random() - 0.5);
     gameData[myRoom].playersReady++;
 
     if (gameData[myRoom].playersReady === 2) {
-        // Start the game for both players
+        if (gameData[myRoom].mode === 'combined') {
+            const all = [];
+            for (let id in gameData[myRoom].playerDares) {
+                all.push(...gameData[myRoom].playerDares[id]);
+            }
+            gameData[myRoom].combinedDares = all.sort(() => Math.random() - 0.5);
+        }
+
         io.to(myRoom).emit('startActualGame'); 
         
-        // Announce that it is Player 1's turn using their ID
-        io.to(myRoom).emit('updateTurn', gameData[myRoom].players[gameData[myRoom].turnIndex]);
+        // NEW: Send both the active ID and the dictionary of names
+        io.to(myRoom).emit('updateTurn', {
+            activePlayerId: gameData[myRoom].players[gameData[myRoom].turnIndex],
+            names: gameData[myRoom].playerNames
+        });
     }
   });
 
-  // --- NEW CODE: THE GAMEPLAY LOOP ---
+  socket.on('addExtraDare', (newDare) => {
+      if (!gameData[myRoom]) return;
+      const room = gameData[myRoom];
 
-  // When a player asks for a dare
+      if (room.mode === 'combined') {
+          room.combinedDares.push(newDare);
+          room.combinedDares.sort(() => Math.random() - 0.5); 
+      } else if (room.mode === 'opponent') {
+          if (!room.playerDares[socket.id]) room.playerDares[socket.id] = [];
+          room.playerDares[socket.id].push(newDare);
+          room.playerDares[socket.id].sort(() => Math.random() - 0.5); 
+      }
+
+      io.to(myRoom).emit('deckRestocked', {
+          activePlayerId: room.players[room.turnIndex],
+          names: room.playerNames
+      });
+  });
+
   socket.on('getDare', () => {
-    const daresArray = gameData[myRoom].allDares;
-    
-    // Check if the game is out of dares
-    if (daresArray.length === 0) {
-        io.to(myRoom).emit('gameOver');
-        return;
-    }
+    const room = gameData[myRoom];
+    let chosenDare = null;
 
-    // Pull the very first dare out of the shuffled array (and remove it so it doesn't repeat)
-    const chosenDare = daresArray.shift(); 
+    if (room.mode === 'combined') {
+        if (room.combinedDares.length === 0) {
+            return io.to(myRoom).emit('gameOver');
+        }
+        chosenDare = room.combinedDares.shift();
+    } 
+    else if (room.mode === 'opponent') {
+        const activePlayer = room.players[room.turnIndex];
+        const opponentId = room.players.find(id => id !== activePlayer);
+        
+        if (!room.playerDares[opponentId] || room.playerDares[opponentId].length === 0) {
+            return io.to(myRoom).emit('gameOver');
+        }
+        chosenDare = room.playerDares[opponentId].shift();
+    }
     
-    // Show it to both players
     io.to(myRoom).emit('showDare', chosenDare);
   });
 
-  // When a player finishes a dare and clicks 'Next Turn'
   socket.on('nextTurn', () => {
-    // Switch the turn index: If it's 0, make it 1. If it's 1, make it 0.
     if (gameData[myRoom].turnIndex === 0) {
         gameData[myRoom].turnIndex = 1;
     } else {
         gameData[myRoom].turnIndex = 0;
     }
-
-    // Announce the new turn to both players!
-    io.to(myRoom).emit('updateTurn', gameData[myRoom].players[gameData[myRoom].turnIndex]);
+    
+    io.to(myRoom).emit('updateTurn', {
+        activePlayerId: gameData[myRoom].players[gameData[myRoom].turnIndex],
+        names: gameData[myRoom].playerNames
+    });
   });
 
 });
 
-// http.listen(3000, () => {
-//   console.log('Server is running on http://localhost:3000');
-// });
-
-// The cloud will provide a port, OR it will use 3000 if you are testing on your computer
 const PORT = process.env.PORT || 3000;
-
 http.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
